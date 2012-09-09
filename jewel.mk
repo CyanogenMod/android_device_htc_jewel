354ca2490ba35bb3003a5025a8057a8c31762aaa
Ethan Chen <intervigil@gmail.com>
2012-09-08 17:04:58 -0700

    jewel: clean up msm_snd_soc
    remove snd_soc_msm
    remove beats entries
    remove VZWUSB entries
    convert from dos to unix formatting
    
    Change-Id: I7d18fe2e6625055cffe53acd80a540ba1a311c23

--- jewel.mk
+++ jewel.mk
@@ -64,7 +64,6 @@ PRODUCT_COPY_FILES += \
     device/htc/jewel/dsp/soundimage/srs_global.cfg:system/etc/soundimage/srs_global.cfg
 
 PRODUCT_COPY_FILES += \
-    device/htc/jewel/dsp/snd_soc_msm/snd_soc_msm:/system/etc/snd_soc_msm/snd_soc_msm \
     device/htc/jewel/dsp/snd_soc_msm/snd_soc_msm_2x:/system/etc/snd_soc_msm/snd_soc_msm_2x
 
 # Keylayouts and Keychars
