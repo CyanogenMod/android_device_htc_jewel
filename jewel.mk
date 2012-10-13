8270b7e12537c273b9dfa2c6b4e40293f018ef01
kushdeck <kushdeck@gmail.com>
2012-10-13 07:33:07 -0700

    jewel: clean up system.prop and build prop overrides.
    Change-Id: I43beabfaa89d655023a1e042ab1d9438f65c55dd

--- jewel.mk
+++ jewel.mk
@@ -127,19 +127,13 @@ PRODUCT_COPY_FILES += \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
-## CDMA Sprint stuffs
+## CDMA Sprint stuffs		
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase=android-sprint-us \
-    ro.com.google.locationfeatures=1 \
-    ro.cdma.home.operator.numeric=310120 \
-    ro.cdma.home.operator.alpha=Sprint
-
-# Extra properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.setupwizard.enable_bypass=1 \
-    ro.com.google.locationfeatures=1
-    dalvik.vm.lockprof.threshold=500
-    dalvik.vm.dexopt-flags=m=y
+    ro.com.google.clientidbase=android-sprint-us \		
+    ro.com.google.locationfeatures=1 \		
+    ro.cdma.home.operator.numeric=310120 \		
+    ro.cdma.home.operator.alpha=Sprint \
+    ro.carrier=Sprint
 
 # We have enough space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
