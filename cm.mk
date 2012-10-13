cb424cac172d5715079355e8fa8cd8c806bd459b
kushdeck <kushdeck@gmail.com>
2012-10-13 07:47:26 -0700

    jewel: update fingerprint from stock 2.13 RUU
    Change-Id: I7fb32d9db247ec00bf96f2b9ca14a1f481bf118e

--- cm.mk
+++ cm.mk
@@ -18,7 +18,7 @@ PRODUCT_MODEL := EVO
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_jewel BUILD_ID=IML74K BUILD_FINGERPRINT=sprint_wwe/jewel/jewel:4.0.3/IML74K/78702.3:user/release-keys PRIVATE_BUILD_DESC="1.22.651.3 CL78702 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_jewel BUILD_ID=IMM76D BUILD_FINGERPRINT="sprint_wwe/jewel/jewel:4.0.4/IMM76D/99866.1:user/release-keys" PRIVATE_BUILD_DESC="2.13.651.1 CL99866 release-keys"
 
 # Release name
 PRODUCT_RELEASE_NAME := jewel
