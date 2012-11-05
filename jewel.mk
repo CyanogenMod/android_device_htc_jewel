b0bde8a1db100b4c8db9e3e5c1ab8aa2db637806
Michael Wawrzyniak <nivron@gmail.com>
2012-11-05 11:39:43 -0500

    Update jewel.mk to override inherited PRODUCT variables.
    This adds PRODUCT_NAME and other PRODUCT variables to override the
    default ones inherited by calling other makefiles.
    
    This also eliminates errors caused during make clobber referencing
    PRODUCT_NAME already being defined when having more than one device
    variant such as jewel and evita.
    
    Change-Id: I74181ede1d94c9142151da1a69294a9a47ef20d6

--- jewel.mk
+++ jewel.mk
@@ -153,3 +153,10 @@ $(call inherit-product-if-exists, vendor/htc/jewel/jewel-vendor.mk)
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_DEVICE := jewel
+PRODUCT_NAME := jewel
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := EVO
+PRODUCT_MANUFACTURER := HTC
