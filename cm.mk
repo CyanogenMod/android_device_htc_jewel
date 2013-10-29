ae0c0d4178cb124009bab757d3fec8e744635a81
Ethan Chen <intervigil@gmail.com>
2013-10-28 18:14:14 -0700

    jewel: remove CDMA APN list
    Change-Id: I19220f2781d4c1c64c6595b64af8e14c1f95fa6f

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
