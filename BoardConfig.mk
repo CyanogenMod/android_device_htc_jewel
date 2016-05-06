c8fa4e45b0ac60c3a3c7f4a2fa3a81bf80602ec2
Ethan Chen <intervigil@gmail.com>
2016-05-05 22:14:50 -0700

    jewel: Remove deprecated vold flags
    Change-Id: I1a983761f0126aa55f53d578eb865b95e1531998

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,9 +40,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/jewel/bluetooth
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 
-# USB
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
 #dev:        size     erasesize name
 #mmcblk0p23: 000ffa00 00000200 "misc"
 #mmcblk0p22: 00fffe00 00000200 "recovery"
@@ -78,9 +75,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 12616466432
 BOARD_CACHEIMAGE_PARTITION_SIZE := 234880000
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 40
 
 # Recovery
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
