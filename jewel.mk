f961785435cbd09782df314e6429cca1e0e96f07
Ethan Chen <intervigil@gmail.com>
2016-05-05 22:19:44 -0700

    jewel: Remove extra AAPT_CONFIG
    Change-Id: I197266897197d0ea59c227e62087ffc49bfbed08

--- jewel.mk
+++ jewel.mk
@@ -75,7 +75,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_LOCALES += en_US
 
